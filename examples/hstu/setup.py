8886ec625dc81a28f7e9e70c489577fda9a2adcf
Runchu Zhao <zhaorunchu@gmail.com>
2025-07-02 09:32:15 +0800

    Add a customized cuda concat_2d_jagged_tensors op (#42)
    * Add cuda_ops folder and cuda method for jagged_2D_tensor_concat
    
    * Move setup.py from hstu/ops/cuda_ops/ to hstu/
    
    * Make jagged_2D_tensor_concat support more dtype and add the corresponding tests
    
    * Add assert for input tensor's dtype, contiguous and device
    
    * Add triton test
    
    * Change total_length calculating way and triton api
    
    * Change kernel type dispatch Macro to pytorch provided version
    
    * Add a optimized kernel but performance keep the same(?)
    
    * Modify optv2 kernel(not work) step1
    
    * Modify optv2 kernel(not work) step2
    
    * Add compute block workloads kernel
    
    * Finish kernel v4 and pass the test
    
    * Remove some print commands and comments
    
    * Add optimized backward kernel step1
    
    * Add optimized backward kernel step2
    
    * Adjust test fuction structure step1
    
    * Add benchmark print message
    
    * Precommit
    
    * Translate comments to English
    
    * Translate comments to English
    
    * Add gemm before benchmark to squish kernel timeline
    
    * Add block loop to handle large input size with large totalblocks number
    
    * Add manual_seed(0)
    
    * Add __launch_bounds__ tag to kernel
    
    * Add __launch_bounds__ tag to kernel step2
    
    * Split kernel
    
    * Change some API steps
    
    * Add backward part kernel
    
    * Add backward part kernel step2
    
    * Move kernel launch configurations to python end
    
    * Delete max_seqlen var in API
    
    * Change max_seqlen calculate way to avoid cpu and gpu sync
    
    * WIP
    
    * fix building error
    
    * Finish cudaLaunchKernel part for forward and backward and pass all tests
    
    * Change the logic in the jaggedconcat part of hstu_block.py and pass the test
    
    * Change the logic in the jaggedconcat part of hstu_block.py and pass the test step2
    
    * Mv test to test path and add install command to Dockerfile
    
    * Remove some unused message
    
    * Add get rank and set device to test file
    
    * Add CUDAGuard on cpp end
    
    * Add corner case when max seq len== 0
    
    * Precommit
    
    ---------
    
    Co-authored-by: Runchu Zhao <runchuz@nvidia.com>
    Co-authored-by: aleliu <aleliu@nvidia.com>

---
+++ examples/hstu/setup.py
@@ -0,0 +1,44 @@
+import os
+
+from setuptools import setup
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+
+
+def nvcc_threads_args():
+    nvcc_threads = os.getenv("NVCC_THREADS") or "4"
+    return ["--threads", nvcc_threads]
+
+
+nvcc_flags = [
+    "-g",
+    "-O3",
+    "-std=c++17",
+    "-U__CUDA_NO_HALF_OPERATORS__",
+    "-U__CUDA_NO_HALF_CONVERSIONS__",
+    "-U__CUDA_NO_BFLOAT16_OPERATORS__",
+    "-U__CUDA_NO_BFLOAT16_CONVERSIONS__",
+    "-U__CUDA_NO_BFLOAT162_OPERATORS__",
+    "-U__CUDA_NO_BFLOAT162_CONVERSIONS__",
+    "--expt-relaxed-constexpr",
+    "--expt-extended-lambda",
+    "--use_fast_math",
+]
+
+setup(
+    name="hstu_cuda_ops",
+    description="HSTU CUDA ops",
+    ext_modules=[
+        CUDAExtension(
+            name="hstu_cuda_ops",
+            sources=[
+                "ops/cuda_ops/csrc/jagged_tensor_op_cuda.cpp",
+                "ops/cuda_ops/csrc/jagged_tensor_op_kernel.cu",
+            ],
+            extra_compile_args={
+                "cxx": ["-O3", "-std=c++17"],
+                "nvcc": nvcc_threads_args() + nvcc_flags,
+            },
+        )
+    ],
+    cmdclass={"build_ext": BuildExtension},
+)

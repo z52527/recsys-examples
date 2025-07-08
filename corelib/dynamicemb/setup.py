c65cc97f59b4adfea814e5985a2ada828a77fb9f
shijie liu <aleliu@nvidia.com>
2025-07-08 09:17:10 +0800

    add back missing comma in dynamicemb setup.py (#89)

--- corelib/dynamicemb/setup.py
+++ corelib/dynamicemb/setup.py
@@ -98,7 +98,7 @@ def get_extensions():
             "-U__CUDA_NO_HALF_OPERATORS__",
             "-U__CUDA_NO_HALF_CONVERSIONS__",
             "-U__CUDA_NO_HALF2_OPERATORS__",
-            "-U__CUDA_NO_BFLOAT16_CONVERSIONS__"
+            "-U__CUDA_NO_BFLOAT16_CONVERSIONS__",
             "-UCUDA_ERROR_CHECK",  # this is to disable HKV error check
         ],
     }
